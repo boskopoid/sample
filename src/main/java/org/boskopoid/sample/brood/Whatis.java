7fdb5977eba4005b5583f38a0f4bfb09365185ab
david <david@david-VPCEA35FG>
2017-03-30 23:30:41 +0800

    Broken class

--- src/main/java/org/boskopoid/sample/brood/Whatis.java
+++ src/main/java/org/boskopoid/sample/brood/Whatis.java
@@ -4,7 +4,7 @@ public class Whatis {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HEY I am Boskopoid");
+		System.out.print ln("HEY I am Boskopoid");
 	}
 
 }
